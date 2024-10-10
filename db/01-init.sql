06e46e2c7f99f0a9d526c56230f67d41aae97846
Nekoru21 <80133990+MoonFoxCake@users.noreply.github.com>
2024-10-09 18:02:01 -0600

    Add files via upload

--- db/01-init.sql
+++ db/01-init.sql
@@ -1,13 +1,8 @@
 -- creates database For postgres
-CREATE DATABASE tasks;
+CREATE DATABASE UsersSM;
 
-\c tasks
+\c UsersSM
 
-
-CREATE TABLE Roles (
-    RolID SERIAL PRIMARY KEY,
-    Permiso VARCHAR(255) NOT NULL
-);
 
 CREATE TABLE Users (
     UserID SERIAL PRIMARY KEY,
@@ -17,21 +12,3 @@ CREATE TABLE Users (
     Role INT REFERENCES Roles(RolID) NOT NULL
 );
 
-CREATE TABLE Posts (
-    PostID SERIAL PRIMARY KEY,
-    AuthorID INT REFERENCES Users(UserID) NOT NULL
-);
-
-CREATE TABLE PostDetails (
-    PostDetailID SERIAL PRIMARY KEY,
-    PostID INT REFERENCES Posts(PostID) NOT NULL,
-    Texto TEXT
-);
-
-CREATE TABLE Media (
-    MediaID SERIAL PRIMARY KEY,
-    PostID INT REFERENCES Posts(PostID) NOT NULL,
-    MediaType VARCHAR(15) NOT NULL,  -- 'image' or 'video'
-    MediaURL VARCHAR(255) NOT NULL,
-    Caption VARCHAR(255)
-);
